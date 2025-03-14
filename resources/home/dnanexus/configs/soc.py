fa7ce23f84a27b70aa442438eaf86e6f0e5a793a
Yujin Kim <yujin.kim@hotmail.fr>
2025-03-14 14:46:07 +0000

    tuple-ise data

--- resources/home/dnanexus/configs/soc.py
+++ resources/home/dnanexus/configs/soc.py
@@ -44,25 +44,25 @@ CONFIG = {
         ("F", 26),
     ),
     "cells_to_colour": [
-        ["C3", PatternFill(patternType="solid", start_color="90EE90")],
-        ["D3", PatternFill(patternType="solid", start_color="90EE90")],
-        ["E3", PatternFill(patternType="solid", start_color="90EE90")],
-        ["F3", PatternFill(patternType="solid", start_color="90EE90")],
-        ["C4", PatternFill(patternType="solid", start_color="90EE90")],
-        ["D4", PatternFill(patternType="solid", start_color="90EE90")],
-        ["E4", PatternFill(patternType="solid", start_color="90EE90")],
-        ["F4", PatternFill(patternType="solid", start_color="90EE90")],
+        ("C3", PatternFill(patternType="solid", start_color="90EE90")),
+        ("D3", PatternFill(patternType="solid", start_color="90EE90")),
+        ("E3", PatternFill(patternType="solid", start_color="90EE90")),
+        ("F3", PatternFill(patternType="solid", start_color="90EE90")),
+        ("C4", PatternFill(patternType="solid", start_color="90EE90")),
+        ("D4", PatternFill(patternType="solid", start_color="90EE90")),
+        ("E4", PatternFill(patternType="solid", start_color="90EE90")),
+        ("F4", PatternFill(patternType="solid", start_color="90EE90")),
     ],
-    "borders": [
-        [
+    "borders": {
+        "single_cells": [
             # generate list of letter and numbers from C-F and 1-8
             # i.e. C1, C2, C3 ..
-            [f"{string.ascii_uppercase[i]}{j}", THIN_BORDER]
+            (f"{string.ascii_uppercase[i]}{j}", THIN_BORDER)
             for i in range(2, 6)
             for j in range(1, 9)
-        ],
-        [["A1", LOWER_BORDER], ["A8", LOWER_BORDER], ["A12", LOWER_BORDER]],
-    ],
+        ]
+        + [("A1", LOWER_BORDER), ("A8", LOWER_BORDER), ("A12", LOWER_BORDER)],
+    },
     "dropdowns": {
         (f"D{i}" for i in range(3, 9)): (
             '"FISH,IHC,NGS,Sanger,NGS multi-gene panel,'
