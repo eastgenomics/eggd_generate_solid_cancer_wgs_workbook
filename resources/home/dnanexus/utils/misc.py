fd7122aa39b8f539d8eb61bcc0e491e365544138
Yujin Kim <yujin.kim@hotmail.fr>
2025-03-14 13:08:38 +0000

    utility function to import modules dynamically

---
+++ resources/home/dnanexus/utils/misc.py
@@ -0,0 +1,35 @@
+import importlib
+from pathlib import Path
+from types import ModuleType
+from typing import Optional
+
+if Path("/home/dnanexus").exists():
+    CONFIG_PATH = Path("/home/dnanexus/configs")
+else:
+    CONFIG_PATH = Path("resources/home/dnanexus/configs")
+
+
+def select_config(name_config: str) -> Optional[ModuleType]:
+    """Given a config name, import the appropriate module for writing the sheet
+
+    Parameters
+    ----------
+    name_config : str
+        Name of the config module to import
+
+    Returns
+    -------
+    Optional[ModuleType]
+        Module to use for config information for writing the sheet or None if
+        the names do not matches
+    """
+
+    for config in CONFIG_PATH.glob("*.py"):
+        module_name = config.name.strip(".py")
+
+        if name_config.lower() == module_name:
+            return importlib.import_module(
+                f".{module_name}", f"{config.parts[-2]}"
+            )
+
+    return None
