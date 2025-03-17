f62f449a4cdfb35ffa98635d6006aebf8f3309ab
Yujin Kim <yujin.kim@hotmail.fr>
2025-03-17 10:04:44 +0000

    conditional writing depending on config content

--- resources/home/dnanexus/utils/excel.py
+++ resources/home/dnanexus/utils/excel.py
@@ -1,6 +1,7 @@
 import openpyxl
 from openpyxl.styles import Alignment, DEFAULT_FONT, Font
 from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.worksheet.worksheet import Worksheet
 import pandas as pd
 
 from utils import misc
@@ -51,62 +52,156 @@ def write_sheet(
     type_config = misc.select_config(sheet_name)
     assert type_config, "Config file couldn't be imported"
 
-    for table in type_config.CONFIG["tables"]:
+    if type_config.CONFIG.get("tables"):
+        write_tables(sheet, type_config.CONFIG["tables"])
+
+    if type_config.CONFIG.get("to_merge"):
+        # merge columns that have longer text
+        sheet.merge_cells(**type_config.CONFIG["to_merge"])
+
+    if type_config.CONFIG.get("to_align"):
+        align_cells(sheet, type_config.CONFIG["to_align"])
+
+    if type_config.CONFIG.get("to_bold"):
+        bold_cells(sheet, type_config.CONFIG["to_bold"])
+
+    if type_config.CONFIG.get("col_width"):
+        set_col_width(sheet, type_config.CONFIG["col_width"])
+
+    if type_config.CONFIG.get("cells_to_colour"):
+        color_cells(sheet, type_config.CONFIG["cells_to_colour"])
+
+    if type_config.CONFIG.get("borders"):
+        draw_borders(sheet, type_config.CONFIG["borders"])
+
+    if type_config.CONFIG.get("dropdowns"):
+        generate_dropdowns(sheet, type_config.CONFIG["dropdowns"])
+
+    return sheet
+
+
+def write_tables(sheet: Worksheet, config_data):
+    """Write the tables from the config
+
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet to write the tables into
+    config_data : list
+        List of tables to write
+    """
+
+    for table in config_data:
         headers = table["headers"]
 
         for cell_x, cell_y in headers:
             value = headers[cell_x, cell_y]
             sheet.cell(cell_x, cell_y).value = value
 
-    if type_config.CONFIG.get("to_merge"):
-        # merge columns that have longer text
-        sheet.merge_cells(**type_config.CONFIG.get("to_merge"))
 
-    # align cells
-    for cell in type_config.CONFIG["to_align"]:
+def align_cells(sheet: Worksheet, config_data):
+    """For given list of cells, align the cells
+
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet in which to align the cells
+    config_data : list
+        List of cells to align
+    """
+
+    for cell in config_data:
         sheet[cell].alignment = Alignment(wrapText=True, horizontal="center")
-        if cell != "C1":
-            sheet[cell].font = Font(italic=True)
 
-    # titles to set to bold
-    for cell in type_config.CONFIG["to_bold"]:
+
+def bold_cells(sheet: Worksheet, config_data):
+    """Given a list of cells, bold them
+
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet in which to bold the cells
+    config_data : list
+        List of cells to bold
+    """
+
+    for cell in config_data:
         sheet[cell].font = Font(bold=True, name=DEFAULT_FONT.name)
 
-    # set column widths for readability
-    for cell, width in type_config.CONFIG["col_width"]:
+
+def set_col_width(sheet: Worksheet, config_data):
+    """Given a list of columns, set their width
+
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet in which to set the width
+    config_data : list
+        List of tuple with the column and its width to set
+    """
+
+    for cell, width in config_data:
         sheet.column_dimensions[cell].width = width
 
-    # colour cells
-    for cell, color in type_config.CONFIG["cells_to_colour"]:
+
+def color_cells(sheet: Worksheet, config_data):
+    """Given a list of cells and their color, color the cells appropriately
+
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet to color the cells in
+    config_data : list
+        List of tuples with the cells and their color
+    """
+
+    for cell, color in config_data:
         sheet[cell].fill = color
 
-    if type_config.CONFIG.get("borders"):
-        if type_config.CONFIG.get("borders").get("single_cells"):
-            # set borders around table areas
-            for cells, type_border in type_config.CONFIG["borders"][
-                "single_cells"
-            ]:
-                sheet[cell].border = type_border
 
-        if type_config.CONFIG.get("borders").get("cell_rows"):
-            for cell_range, type_border in type_config.CONFIG.get(
-                "borders"
-            ).get("cell_rows"):
-                for cells in sheet[cell_range]:
-                    for cell in cells:
-                        cell.border = type_border
+def draw_borders(sheet: Worksheet, config_data):
+    """Draw borders around the cells
 
-    for cells, options in type_config.CONFIG["dropdowns"].items():
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet in which to draw borders
+    config_data : dict
+        Dict containing info for the single cells to draw borders around and
+        the rows of cells
+    """
+
+    if config_data.get("single_cells"):
+        for cell, type_border in config_data["single_cells"]:
+            sheet[cell].border = type_border
+
+    if config_data.get("cell_rows"):
+        for cell_range, type_border in config_data["cell_rows"]:
+            for cells in sheet[cell_range]:
+                for cell in cells:
+                    cell.border = type_border
+
+
+def generate_dropdowns(sheet: Worksheet, config_data):
+    """Write in the dropdown menus
+
+    Parameters
+    ----------
+    sheet : Worksheet
+        Worksheet in which to write the dropdown menus
+    config_data : dict
+        Dict of data for the dropdown menus
+    """
+
+    for cells, options in config_data["cells"].items():
         dropdown = DataValidation(
             type="list", formula1=options, allow_blank=True
         )
         dropdown.prompt = "Select from the list"
-        dropdown.promptTitle = ""
+        dropdown.promptTitle = config_data["title"]
         dropdown.showInputMessage = True
         dropdown.showErrorMessage = True
         sheet.add_data_validation(dropdown)
 
         for cell in cells:
             dropdown.add(sheet[cell])
-
-    return sheet
