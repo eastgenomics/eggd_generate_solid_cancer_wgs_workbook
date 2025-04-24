4e1f52f8c6cdb066c9d89bace00989c34c2ecd0d
Yujin Kim <yujin.kim@hotmail.fr>
2025-04-24 16:14:27 +0100

    adjust loss sheet

--- resources/home/dnanexus/configs/loss.py
+++ resources/home/dnanexus/configs/loss.py
@@ -5,6 +5,8 @@ from openpyxl.styles.fills import PatternFill
 from openpyxl.utils.dataframe import dataframe_to_rows
 import pandas as pd
 
+from utils import misc
+
 THIN = Side(border_style="thin", color="000000")
 THIN_BORDER = Border(left=THIN, right=THIN, top=THIN, bottom=THIN)
 
@@ -15,8 +17,9 @@ CONFIG = {
         for i, value in enumerate(
             [
                 "Event domain",
-                "Impacted transcript region",
                 "Gene",
+                "RefSeq IDs",
+                "Impacted transcript region",
                 "GRCh38 coordinates",
                 "Type",
                 "Copy Number",
@@ -43,38 +46,27 @@ CONFIG = {
             1,
         )
     },
-    "to_bold": [f"{string.ascii_uppercase[i]}1" for i in range(0, 25)],
+    "to_bold": [f"{misc.convert_index_to_letters(i)}1" for i in range(0, 26)],
     "col_width": [
         ("B", 12),
         ("C", 16),
-        ("D", 22),
-        ("E", 14),
-        ("G", 16),
+        ("D", 16),
+        ("E", 22),
         ("H", 14),
-        ("I", 22),
-        ("J", 20),
-        ("K", 20),
-        ("L", 20),
-        ("M", 22),
-        ("N", 20),
-        ("O", 16),
-        ("P", 16),
-        ("Q", 16),
-        ("R", 16),
-        ("S", 16),
-        ("T", 16),
-        ("U", 16),
-        ("V", 16),
-        ("W", 16),
-        ("X", 16),
+        ("I", 10),
+        ("J", 10),
     ],
     "borders": {
         "cell_rows": [
-            ("A1:Y1", THIN_BORDER),
+            ("A1:Z1", THIN_BORDER),
         ],
     },
-    "auto_filter": "A:Y",
-    "freeze_panes": "E1",
+    "row_height": [(1, 80)],
+    "auto_filter": "A:Z",
+    "freeze_panes": "F1",
+    "text_orientation": [
+        (f"{misc.convert_index_to_letters(i)}1", 90) for i in range(11, 26)
+    ],
 }
 
 
@@ -108,23 +100,23 @@ def add_dynamic_values(data: pd.DataFrame) -> dict:
                 f"{col}{i}",
                 PatternFill(patternType="solid", start_color="FFDBBB"),
             )
-            for col in ["K", "L", "M"]
+            for col in ["L", "M", "N"]
             for i in range(1, nb_sv_variants + 2)
         ]
         + [
             (
-                # letters M to R
+                # letters O to Z
                 f"{string.ascii_uppercase[i]}{j}",
                 PatternFill(patternType="solid", start_color="c4d9ef"),
             )
-            for i in range(13, 25)
+            for i in range(14, 26)
             for j in range(1, nb_sv_variants + 2)
         ],
-        "to_align": [f"F{i}" for i in range(2, nb_sv_variants + 2)],
+        "to_align": [f"G{i}" for i in range(2, nb_sv_variants + 2)],
         "dropdowns": [
             {
                 "cells": {
-                    (f"K{i}" for i in range(2, nb_sv_variants + 2)): (
+                    (f"L{i}" for i in range(2, nb_sv_variants + 2)): (
                         '"Oncogenic, Likely oncogenic,'
                         "Uncertain, Likely passenger,"
                         'Likely artefact"'
