8ce0970898298d35f2db40f642cc62321877e9b0
Yujin Kim <yujin.kim@hotmail.fr>
2025-05-13 15:36:14 +0100

    upload output file and add new inputs

--- resources/home/dnanexus/main.sh
+++ resources/home/dnanexus/main.sh
@@ -8,12 +8,14 @@ main() {
     python3 /home/dnanexus/generate_workbook.py \
         -hs in/hotspots/* \
         -r in/reference_gene_groups/* \
+        -p in/panelapp/* \
+        -cb in/cytological_bands/* \
         -c in/clinvar/* \
         -i in/clinvar_index/* \
         -html in/supplementary_html/* \
         -rv in/reported_variants/* \
         -rsv in/reported_structural_variants/*
 
-    file_id=$(dx upload "output.xlsx" --brief)
+    file_id=$(dx upload "output/$(ls output/)" --brief)
     dx-jobutil-add-output workbook $file_id
 }
