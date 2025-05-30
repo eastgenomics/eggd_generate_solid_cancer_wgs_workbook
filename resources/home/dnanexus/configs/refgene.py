8423d54203076532f6a8d48bec1e27d283f71767
Yujin Kim <yujin.kim@hotmail.fr>
2025-05-30 14:09:49 +0100

    move and fix comment

--- resources/home/dnanexus/configs/refgene.py
+++ resources/home/dnanexus/configs/refgene.py
@@ -99,10 +99,10 @@ def add_dynamic_values(df: pd.DataFrame) -> dict:
             (1, i): column for i, column in enumerate(df.columns, 1)
         }
         | {
-            # remove the col and row index from the writing?
             (r_idx - 1, c_idx - 1): value
             for r_idx, row in enumerate(dataframe_to_rows(df), 1)
             for c_idx, value in enumerate(row, 1)
+            # remove the col and row index from the writing
             if c_idx != 1 and r_idx != 1
         },
         "cells_to_colour": [
